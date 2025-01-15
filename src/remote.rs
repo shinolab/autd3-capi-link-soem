a5c3e4d546937c178d170e25667f07dd1888b97c
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-01-16 08:51:34 +0900

    bump to v29.0.0-rc.16

--- src/remote.rs
+++ src/remote.rs
@@ -6,11 +6,11 @@ use autd3_link_soem::remote::*;
 
 #[no_mangle]
 #[must_use]
-pub unsafe extern "C" fn AUTDLinkRemoteSOEM(addr: *const c_char) -> ResultSyncLinkBuilder {
+pub unsafe extern "C" fn AUTDLinkRemoteSOEM(addr: *const c_char) -> ResultLinkBuilder {
     let addr = if addr.is_null() {
         ""
     } else {
-        validate_cstr!(addr, SyncLinkBuilderPtr, ResultSyncLinkBuilder)
+        validate_cstr!(addr, LinkBuilderPtr, ResultLinkBuilder)
     };
     addr.parse::<SocketAddr>().map(RemoteSOEM::builder).into()
 }
